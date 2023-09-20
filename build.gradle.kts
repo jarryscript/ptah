5e559869e64f92993cdc9aa2182359b752f81073
Jarry Zhou <jarry_zhou@epam.com>
2023-09-20 13:55:32 +0800

    update pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.3"
     id("io.spring.dependency-management") version "1.1.3"
+    id("org.liquibase.gradle") version "2.0.4"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
     kotlin("plugin.jpa") version "1.8.22"
+
 }
 
 group = "com"
